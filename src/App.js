609917931048e9fd5b9990250902e46682a55672
zzzmini <sm01079200887@gmail.com>
2025-03-26 17:43:04 +0900

    #1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
 function App() {
+  const [currentTime, setCurrentTime] = useState('');
+
+  useEffect(()=>{
+    // /localhost:8080/api/time GET
+    axios.get('/api/time')
+    .then(respose => {
+      setCurrentTime(respose.data);
+    })
+    .catch(error => {
+      console.log('API 호출 중 오류 발생', error);
+    })
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>현재 시간 </h1>
+      <p>{currentTime}</p>
     </div>
   );
 }
